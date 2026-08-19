f0e7413aeea3289c47d76b1ae90eea4f8b3a0e3d
fazza-abiyyu <fazza_abiyyu@qq.com>
2026-08-20 06:47:18 +0700

    release: v1.1.5 - Revert versionName to 1.1.5 with code 7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to this project will be documented in this file.
 
-## [1.1.6] - 2026-08-20
+## [1.1.5] - 2026-08-20
 
 ### ✨ New Features
 
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 7
-        versionName = "1.1.6"
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
